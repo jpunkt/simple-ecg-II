77925adbe015a559b373b0da82e87cf4868cb168
jpunkt <jpunkt@black-box>
2017-12-14 13:50:04 +0100

    adding clion settings

--- main.c
+++ main.c
@@ -2,18 +2,20 @@
 #include <util/delay.h>
 #include "easyavr.h"
 
-#define LED_PORT PORTD
-#define LED_PIN 0
-#define LED_DDR DDRD
+#define LED_PORT PORTB
+#define LED_PIN 5
+#define LED_DDR DDRB
 
 int main(void)
 {
+    //DDRB = 0xFF;
+
     PIN_AS_OUTPUT(LED_DDR, LED_PIN);
 
     while (1) {
         PIN_ON(LED_PORT, LED_PIN);
-        _delay_ms(100);
+        _delay_ms(1000);
         PIN_OFF(LED_PORT, LED_PIN);
-        _delay_ms(100);
+        _delay_ms(500);
     }
 }
